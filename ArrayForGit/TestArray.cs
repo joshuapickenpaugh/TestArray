ec3669d648fccab352a121ee5dc169121ab2c71e
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-07 12:57:31 -0500

    First Commit (FINAL)

---
+++ ArrayForGit/TestArray.cs
@@ -0,0 +1,36 @@
+﻿//JP, IN2017, 10062015
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07_Ary
+{
+    class TestArray
+    {
+        public string msg { get; set; }
+
+        public TestArray(string message)
+        {
+            msg = message;
+        }
+
+        public void AryDisplay(int[] methARRAY)
+        {
+            Console.WriteLine(this.msg);
+            foreach (int num in methARRAY)
+            {
+                if (num >= 50)
+                {
+                    Console.WriteLine(num);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ ArrayForGit/TestAry.cs
@@ -0,0 +1,49 @@
+﻿//Joshua Pickenpaugh, IN2017, Adv.C#
+//Modulu 07, October 6th, 2015
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayForGit
+{
+    //3b:
+    class TestAry
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 23, 44, 55, 12, 6, 99 };
+
+            //2c:
+            Console.WriteLine("The values in the array are:");
+            for (int a = 0; a < myArray.Length; a++)
+            {
+                Console.WriteLine(myArray[a]);
+            }
+
+            //2d:
+            ArrayMethod(myArray);
+
+            Console.WriteLine();
+
+            //2f:
+            TestArray testAry = new TestArray("Array of values above 50:");
+            //2g:
+            testAry.AryDisplay(myArray);
+
+            Console.ReadKey();
+        }
+
+        public static void ArrayMethod(int[] METHarray)
+        {
+            Console.WriteLine("The array displayed from the user-defined method are: ");
+            //2e:
+            for (int a = 0; a < METHarray.Length; a++)
+            {
+                Console.WriteLine(METHarray[a]);
+            }
+        }
+    }
+}
